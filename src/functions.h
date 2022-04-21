de0bca8ced61422ef3668e6c1c00e4d2f902b6f2
Erkki Moorits <erkki.moorits@mail.ee>
2022-04-21 16:33:39 +0300

    Removed unused function orage_select_date

--- src/functions.h
+++ src/functions.h
@@ -149,9 +149,6 @@ char *orage_cal_to_icaldate(GtkCalendar *cal);
 void orage_move_day(struct tm *t, int day);
 gint orage_days_between (const struct tm *t1, const struct tm *t2);
 
-__attribute__((deprecated ("use orage_select_date2")))
-void orage_select_date(GtkCalendar *cal, guint year, guint month, guint day);
-
 /** Set (GTK) calendar to selected date.
  *  @param cal instance of GTK calendar
  *  @param gdt selected date.
