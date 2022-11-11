3d28258f45ad0c336271268f1d8e028b3cd7e378
Erkki Moorits <erkki.moorits@mail.ee>
2022-11-11 10:15:05 +0200

    Replaced dbus-glib service GDBus analogue

--- src/orage-dbus-object.h
+++ src/orage-dbus-object.h
@@ -23,6 +23,7 @@
 #ifndef __ORAGE_DBUS_OBJECT_H__
 #define __ORAGE_DBUS_OBJECT_H__
 
+#include <glib.h>
 #include <glib-object.h>
 
 G_BEGIN_DECLS
@@ -30,8 +31,7 @@ G_BEGIN_DECLS
 typedef struct _OrageDBusClass OrageDBusClass;
 typedef struct _OrageDBus      OrageDBus;
 
-
-#define ORAGE_DBUS_TYPE             (orage_dbus_get_type())
+#define ORAGE_TYPE_DBUS             (orage_dbus_get_type())
 #define ORAGE_DBUS(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj), ORAGE_TYPE_DBUS, OrageDBus))
 #define ORAGE_DBUS_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST((klass), ORAGE_TYPE_DBUS, OrageDBusClass))
 #define ORAGE_IS_DBUS(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), ORAGE_TYPE_DBUS))
@@ -39,22 +39,7 @@ typedef struct _OrageDBus      OrageDBus;
 #define ORAGE_DBUS_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj), ORAGE_TYPE_DBUS, OrageDBustClass))
 
 GType orage_dbus_get_type(void);
-
-gboolean orage_dbus_service_load_file(DBusGProxy *proxy
-                , const char *IN_file
-                , GError **error);
-gboolean orage_dbus_service_export_file(DBusGProxy *proxy
-                , const char *IN_file, const gint IN_type, const char *IN_uids
-                , GError **error);
-gboolean orage_dbus_service_add_foreign(DBusGProxy *proxy
-                , const char *IN_file, const gboolean IN_mode, const char *IN_name
-                , GError **error);
-gboolean orage_dbus_service_remove_foreign(DBusGProxy *proxy
-                , const char *IN_file
-                , GError **error);
-
 void orage_dbus_start(void);
-
 
 G_END_DECLS
 
