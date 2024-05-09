2718866a333526bd9737abccc2c06c4347568966
Erkki Moorits <erkki.moorits@mail.ee>
2024-05-09 10:05:42 +0300

    Replaced day_win with OrageWeekWindow

--- src/orage-week-window.h
+++ src/orage-week-window.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Erkki Moorits
+ * Copyright (c) 2021-2024 Erkki Moorits
  * Copyright (c) 2005-2013 Juha Kautto  (juha at xfce.org)
  * Copyright (c) 2004-2006 Mickael Graf (korbinus at xfce.org)
  *
@@ -20,62 +20,14 @@
  *     Boston, MA 02110-1301 USA
  */
 
-#ifndef __DAY_VIEW_H__
-#define __DAY_VIEW_H__
+#ifndef ORAGE_WEEK_WINDOW_H
+#define ORAGE_WEEK_WINDOW_H 1
 
-#define MAX_DAYS 40
+#include <glib-object.h>
+#include <glib.h>
+#include <gtk/gtk.h>
 
-typedef struct _day_win
-{
-    GtkAccelGroup *accel_group;
-
-    GtkWidget *Window;
-    GtkWidget *Vbox;
-
-    GtkWidget *Menubar;
-    GtkWidget *File_menu;
-    GtkWidget *File_menu_new;
-    GtkWidget *File_menu_close;
-    GtkWidget *View_menu;
-    GtkWidget *View_menu_refresh;
-    GtkWidget *Go_menu;
-    GtkWidget *Go_menu_today;
-    GtkWidget *Go_menu_prev_day;
-    GtkWidget *Go_menu_prev_week;
-    GtkWidget *Go_menu_next_day;
-    GtkWidget *Go_menu_next_week;
-
-    GtkWidget *Toolbar;
-    GtkWidget *Create_toolbutton;
-    GtkWidget *Previous_week_toolbutton;
-    GtkWidget *Previous_day_toolbutton;
-    GtkWidget *Today_toolbutton;
-    GtkWidget *Next_day_toolbutton;
-    GtkWidget *Next_week_toolbutton;
-    GtkWidget *Refresh_toolbutton;
-    GtkWidget *Close_toolbutton;
-
-    GtkWidget *StartDate_button;
-    GtkRequisition StartDate_button_req;
-    GtkWidget *day_spin;
-
-    GtkWidget *scroll_win;
-    GtkWidget *dtable;   /* day table */
-
-    GtkWidget *header[MAX_DAYS];
-    GtkWidget *element[24][MAX_DAYS];
-    GtkWidget *line[24][MAX_DAYS];
-
-    guint upd_timer;
-    gdouble scroll_pos; /* remember the scroll position */
-
-    GList *apptw_list; /* keep track of appointments being updated */
-
-    GDateTime *a_day; /* start date */
-    gint days;      /* how many days to show */
-} day_win;
-
-void refresh_day_win(day_win *dw);
+G_BEGIN_DECLS
 
 #define ORAGE_WEEK_WINDOW_TYPE (orage_week_window_get_type ())
 G_DECLARE_FINAL_TYPE (OrageWeekWindow, orage_week_window, ORAGE, WEEK_WINDOW,
@@ -85,5 +37,18 @@ G_DECLARE_FINAL_TYPE (OrageWeekWindow, orage_week_window, ORAGE, WEEK_WINDOW,
  *  @param date window start date
  */
 OrageWeekWindow *orage_week_window_new (GDateTime *date);
+
+/** Build new week window. This function create new week window and connect all
+ *  necceary signals to close window.
+ *  @param date window start date
+ */
+void orage_week_window_build (GDateTime *date);
+
+void orage_week_window_remove_appointment_window (OrageWeekWindow *self,
+                                                  gconstpointer apptw);
+
+void orage_week_window_refresh (OrageWeekWindow *window);
+
+G_END_DECLS
 
 #endif
